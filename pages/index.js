3e3964861522247b8cbf239c6828767b89e9f0b1
alejandromartinez141-spec <alejandromartinez141@gmail.com>
2025-10-04 20:08:04 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,8 @@
-
+export default function Home() {
+  return (
+    <div style={{textAlign: "center", marginTop: "100px"}}>
+      <h1>🚀 Renobix Pro funcionando correctamente ✅</h1>
+      <p>Si ves esta pantalla, el frontend ya se desplegó bien 😎</p>
+    </div>
+  );
+}
